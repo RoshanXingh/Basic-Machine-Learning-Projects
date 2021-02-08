12795f838f2a9ff3271c1f9c5e4591e45f07104b
RoshanXingh <roshanrajsingh38@outlook.com>
2021-02-08 13:46:14 +0530

    simple regression model

---
+++ Simple Linear Regression/Predicting Salary/predicting_salary.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[: ,0:1]
+y = dataset.iloc[:,-1]
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+y_pred = regressor.predict(x_test)
+
+plt.scatter(x_train, y_train, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue')
+plt.show()
+
+plt.scatter(x_test, y_pred, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue')
+plt.show()
